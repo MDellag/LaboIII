fb4caee14745b9460c3bb2829ec7fe7d113e54a2
sosajuanmanuel <juanmanuelsosa1996@gmail.com>
2020-11-02 10:22:13 -0300

    reuploaded

---
+++ dellagiovanna.PPLABIII3D/scripts/Anuncios.js
@@ -0,0 +1,22 @@
+class Anuncio {
+  constructor(id, titulo, transaccion, descripcion, precio) {
+    this.id = id;
+    this.titulo = titulo;
+    this.transaccion = transaccion;
+    this.descripcion = descripcion;
+    this.precio = precio;
+  }
+
+  AltaAnuncio() {
+    console.log(`Dado de alta: ${this.titulo}`);
+  }
+}
+
+export default class Anuncio_Auto extends Anuncio {
+      constructor(id, titulo, transaccion,descripcion, precio, puertas, kms, potencia){
+        super(id, titulo, transaccion, descripcion, precio)
+        this.puertas = puertas
+        this.kms = kms
+        this.potencia = potencia
+      }
+}
